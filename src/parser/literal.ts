210430e6f6535e57bc12cc2d9726c351194cb562
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2021-03-14 21:09:28 -0300

    Number literal parser

--- src/parser/literal.ts
+++ src/parser/literal.ts
@@ -2,16 +2,20 @@ import Pr, { Parser } from 'pierrejs';
 import { LiteralStatement } from './statements';
 
 export const lNull = Pr.string('null')
-        .map(() => null);
+        .map(() => null)
+        .withName('null');
 
 export const lUndefined = Pr.string('undefined')
-        .map(() => void 0);
+        .map(() => void 0)
+        .withName('undefined');
 
 export const lBoolean = Pr.oneOf(Pr.string('true'), Pr.string('false'))
         .map(s => s === 'true')
         .withName('boolean');
 
-export const lNumber = Pr.fail('Unimplemented');
+export const lNumber = Pr.regex('number', /^[+\-]?[0-9]+(\.[0-9]+)?/)
+        .map(n => Number(n));
+
 export const lString = Pr.fail('Unimplemented');
 
 export const $literal: Parser<LiteralStatement> = Pr.oneOf(lNull, lUndefined, lBoolean, lNumber, lString)
