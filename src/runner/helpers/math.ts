7e1f6dd088d9b9181f546223fa35989a8cac2b05
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2021-10-04 08:59:45 -0300

    More math operator tests

--- src/runner/helpers/math.ts
+++ src/runner/helpers/math.ts
@@ -1,27 +1,27 @@
-function add(a: any, b: any): any {
+function add(a: any, b: any): number {
     return Number(a) + Number(b);
 }
 
-function subtract(a: any, b: any): any {
+function subtract(a: any, b: any): number {
     return Number(a) - Number(b);
 }
 
-function multiply(a: any, b: any): any {
+function multiply(a: any, b: any): number {
     return Number(a) * Number(b);
 }
 
-function divide(a: any, b: any): any {
+function divide(a: any, b: any): number {
     return Number(a) / Number(b);
 }
 
-function modulo(a: any, b: any): any {
+function modulo(a: any, b: any): number {
     return Number(a) % Number(b);
 }
 
 export const mathHelpers = Object.assign(Object.create(null), {
     add, sum: add, plus: add,
     subtract, minus: subtract,
-    multiply, product: multiply,
+    multiply, product: multiply, times: multiply,
     divide, quotient: divide,
-    modulo, remainder: modulo, mod: modulo,  		
+    modulo, remainder: modulo, mod: modulo,
 });
--- test/math-helpers.spec.js
+++ test/math-helpers.spec.js
@@ -63,4 +63,90 @@ describe('helpers', () => { describe('math', () => {
         });
     });
 
+    describe('multiply', () => {
+        it('should multiply numbers', async () => {
+            const templ = compile('{{multiply numA numB}}');
+            expect(await templ({ numA: 2, numB: 3 })).to.equal('6');
+            expect(await templ({ numA: 2, numB: -1 })).to.equal('-2');
+        });
+
+        it('should multiply number strings', async () => {
+            const templ = compile('{{multiply numA numB}}');
+            expect(await templ({ numA: '2', numB: '3' })).to.equal('6');
+            expect(await templ({ numA: '2', numB: '-1' })).to.equal('-2');
+        });
+
+        it('should return NaN for non-numbers', async () => {
+            const templ = compile('{{multiply numA numB}}');
+            expect(await templ({ numA: '2', numB: 'foo' })).to.equal('NaN');
+            expect(await templ({ numA: 'foo', numB: '3' })).to.equal('NaN');
+        });
+
+        it('should work as times', async () => {
+            const templ = compile('{{times numA numB}}');
+            expect(await templ({ numA: 2, numB: 2 })).to.equal('4');
+        });
+
+        it('should work as product', async () => {
+            const templ = compile('{{product numA numB}}');
+            expect(await templ({ numA: 2, numB: 2 })).to.equal('4');
+        });
+    });
+
+    describe('divide', () => {
+        it('should divide numbers', async () => {
+            const templ = compile('{{divide numA numB}}');
+            expect(await templ({ numA: 6, numB: 3 })).to.equal('2');
+            expect(await templ({ numA: 6, numB: -3 })).to.equal('-2');
+        });
+
+        it('should divide number strings', async () => {
+            const templ = compile('{{divide numA numB}}');
+            expect(await templ({ numA: '6', numB: '3' })).to.equal('2');
+            expect(await templ({ numA: '6', numB: '-3' })).to.equal('-2');
+        });
+
+        it('should return NaN for non-numbers', async () => {
+            const templ = compile('{{divide numA numB}}');
+            expect(await templ({ numA: '6', numB: 'foo' })).to.equal('NaN');
+            expect(await templ({ numA: 'foo', numB: '3' })).to.equal('NaN');
+        });
+
+        it('should work as dividedBy', async () => {
+            const templ = compile('{{quotient numA numB}}');
+            expect(await templ({ numA: 6, numB: 3 })).to.equal('2');
+        });
+    });
+
+    describe('modulo', () => {
+        it('should modulo numbers', async () => {
+            const templ = compile('{{modulo numA numB}}');
+            expect(await templ({ numA: 5, numB: 2 })).to.equal('1');
+            expect(await templ({ numA: 12, numB: 3 })).to.equal('0');
+            expect(await templ({ numA: 17, numB: 5 })).to.equal('2');
+        });
+
+        it('should modulo number strings', async () => {
+            const templ = compile('{{modulo numA numB}}');
+            expect(await templ({ numA: '5', numB: '2' })).to.equal('1');
+            expect(await templ({ numA: '12', numB: '3' })).to.equal('0');
+        });
+
+        it('should return NaN for non-numbers', async () => {
+            const templ = compile('{{modulo numA numB}}');
+            expect(await templ({ numA: '6', numB: 'foo' })).to.equal('NaN');
+            expect(await templ({ numA: 'foo', numB: '3' })).to.equal('NaN');
+        });
+
+        it('should work as mod', async () => {
+            const templ = compile('{{mod numA numB}}');
+            expect(await templ({ numA: 5, numB: 2 })).to.equal('1');
+        });
+
+        it('should work as remainder', async () => {
+            const templ = compile('{{remainder numA numB}}');
+            expect(await templ({ numA: 5, numB: 2 })).to.equal('1');
+        });
+    });
+
 }); });
